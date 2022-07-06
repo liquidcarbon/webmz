aaa89ca2e7df6b3c76e4b37f46b1fe6d3ca70f4f
Alexander Kislukhin <47034358+liquidcarbon@users.noreply.github.com>
2022-07-06 17:30:17 -0600

    parse

---
+++ src/io.py
@@ -0,0 +1,29 @@
+# src/io.py
+
+
+
+from pathlib import Path
+from pyodide import JsException
+from pyodide.http import pyfetch
+
+async def pyfetch_download(url, filename=None):
+    """
+
+    Notes
+    -----
+    If the word asyncio appears anywhere in your code, including in a comment,
+    then PyScript will run Python asynchronously using Pyodide’s .runPythonAsync() method.
+    Otherwise, it’ll call the synchronous .runPython() counterpart,
+    which doesn’t let you use the await keyword.
+    """
+
+    filename = filename or Path(url).name
+    try:
+        response = await pyfetch(url)
+        if response.ok:
+            with open(filename, mode="wb") as file:
+                file.write(await response.bytes())
+    except JsException:
+        return None
+    else:
+        return filename
